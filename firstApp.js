4354849dd71d187189bed04dcba77bc19cd5b074
Devina <44644511+Devina6@users.noreply.github.com>
2023-05-24 19:46:47 +0530

    added arrow function to multiply

--- firstApp.js
+++ firstApp.js
@@ -7,3 +7,7 @@ console.log("Hello World");
 //to create a file from js 
 const fs = require ('fs')
 fs.writeFileSync('hello.txt','Hello World');
+
+// arrow function to multiply
+const multiply = (a,b)=> a*b;
+console.log(multiply(2,4));
