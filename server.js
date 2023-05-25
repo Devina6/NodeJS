dff33a3284dc6181692561cf5f232e69cdb30278
Devina <44644511+Devina6@users.noreply.github.com>
2023-05-25 12:14:48 +0530

    view request and return response

--- server.js
+++ server.js
@@ -3,6 +3,18 @@
 
 const http = require('http')//http=global module,./http=local http file
 const server = http.createServer((req,res)=>{
-    console.log('devina')
+    console.log('devina');
+    
+    //view request
+    console.log(req);
+    console.log(req.url,req.method,req.headers);
+   
+    //return a response
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>Hello World</title></head>');
+    res.write('<body><h1>Welcome to my Node js Project</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 server.listen(4000);
