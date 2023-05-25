b4e4a989091dfe6eaa125d72a22faf043f76bb27
Devina <44644511+Devina6@users.noreply.github.com>
2023-05-25 14:12:56 +0530

    read from file and display on browser

--- server.js
+++ server.js
@@ -4,13 +4,25 @@
 const http = require('http')//http=global module,./http=local http file
 const server = http.createServer((req,res)=>{
    const url = req.url;
+    const method = req.method;
+    
     if(url==='/'){
-        res.write('<html>');
-        res.write('<head><title>Enter message</title></head>');
-        res.write("<body><form action='/message' method='POST'><input type='text'><button type='submit'>Send</button></form></body>");
-    res.write('</html>');
-    return res.end();
+        return fs.readFile('message.txt',{encoding:'utf8'},(err,data)=>{
+            if(err){
+                console.log(err);
+            }
+            //console.log('data from file '+data);
+            res.setHeader('Content-Type','text/html')
+            res.write('<html>');
+            res.write('<head><title>Enter message</title></head>');
+            res.write(`<body><h3>${data}</h3></body>`);
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+            res.write('</html>');
+        });
+        
+        return res.end();
     }
+   
     if(url==='/home'){
         res.write('<html>');
         res.write('<head><title>HOME</title></head>');
@@ -18,6 +30,7 @@ const server = http.createServer((req,res)=>{
         res.write('</html>');
         return res.end();
     }
+   
     if(url==='/about'){
         res.write('<html>');
         res.write('<head><title>ABOUT</title></head>');
@@ -25,6 +38,7 @@ const server = http.createServer((req,res)=>{
         res.write('</html>');
         return res.end();
     }
+   
     if (url==='/node'){
         res.write('<html>');
         res.write('<head><title>NODE</title></head>');
@@ -32,20 +46,21 @@ const server = http.createServer((req,res)=>{
         res.write('</html>');
         return res.end();
     }
+    
     if(url==='/message' && method==='POST'){
         const body = [];
-        req.on('data',(chunk)=>{
+        req.on('data',(chunk)=>{ //data is node js eventlistener
             body.push(chunk);
         });
-        req.on('end',()=>{
+        req.on('end',()=>{ //end is node js eventlistener
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt',message);
-        });
-        
-        res.statusCode = 302;
-        res.setHeader('Location','/')
-        return res.end();
+            fs.writeFile('message.txt',message,err=>{
+               res.statusCode = 302;
+               res.setHeader('Location','/')
+               return res.end();
+            });
+        });     
     }
 });
 server.listen(4000);
