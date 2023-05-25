a1afe2af7e2025d1cc67b2615bcd05e86f61254b
Devina <44644511+Devina6@users.noreply.github.com>
2023-05-25 18:45:19 +0530

    link server to routes and extract from routes

--- server.js
+++ server.js
@@ -1,66 +1,8 @@
-//creates a server at port 4000 and consoles in cmd
-//localhost:4000 to call server from browser
+const http = require('http'); 
 
-const http = require('http')//http=global module,./http=local http file
-const server = http.createServer((req,res)=>{
-   const url = req.url;
-    const method = req.method;
-    
-    if(url==='/'){
-        return fs.readFile('message.txt',{encoding:'utf8'},(err,data)=>{
-            if(err){
-                console.log(err);
-            }
-            //console.log('data from file '+data);
-            res.setHeader('Content-Type','text/html')
-            res.write('<html>');
-            res.write('<head><title>Enter message</title></head>');
-            res.write(`<body><h3>${data}</h3></body>`);
-            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-            res.write('</html>');
-        });
-        
-        return res.end();
-    }
-   
-    if(url==='/home'){
-        res.write('<html>');
-        res.write('<head><title>HOME</title></head>');
-        res.write("<body><h1>Welcome Home</h1></body>");
-        res.write('</html>');
-        return res.end();
-    }
-   
-    if(url==='/about'){
-        res.write('<html>');
-        res.write('<head><title>ABOUT</title></head>');
-        res.write("<body><h1>Welcome to About Us Page</h1></body>");
-        res.write('</html>');
-        return res.end();
-    }
-   
-    if (url==='/node'){
-        res.write('<html>');
-        res.write('<head><title>NODE</title></head>');
-        res.write('<body><h1>Welcome to my Node js Project</h1></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    
-    if(url==='/message' && method==='POST'){
-        const body = [];
-        req.on('data',(chunk)=>{ //data is node js eventlistener
-            body.push(chunk);
-        });
-        req.on('end',()=>{ //end is node js eventlistener
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt',message,err=>{
-               res.statusCode = 302;
-               res.setHeader('Location','/')
-               return res.end();
-            });
-        });     
-    }
-});
+const routes = require('./routes');
+
+const server = http.createServer(routes);
+
 server.listen(4000);
+
