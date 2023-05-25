a2bec0fbcd5f142faa5bc7f67eda0b9624764f60
Devina <44644511+Devina6@users.noreply.github.com>
2023-05-25 12:38:27 +0530

    custom responses for home,about,node

--- server.js
+++ server.js
@@ -3,17 +3,38 @@
 
 const http = require('http')//http=global module,./http=local http file
 const server = http.createServer((req,res)=>{
-    console.log('devina');
-    
-    //view request
-    console.log(req);
-    console.log(req.url,req.method,req.headers);
-   
-    //return a response
-    res.setHeader('Content-Type','text/html');
+   const url = req.url;
+    if(url==='/'){
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write("<body><form action='/message' method='POST'><input type='text'><button type='submit'>Send</button></form></body>");
+    res.write('</html>');
+    return res.end();
+    }
+    if(url==='/home'){
+        res.write('<html>');
+        res.write('<head><title>HOME</title></head>');
+        res.write("<body><h1>Welcome Home</h1></body>");
+        res.write('</html>');
+        return res.end();
+    }
+    if(url==='/about'){
+        res.write('<html>');
+        res.write('<head><title>ABOUT</title></head>');
+        res.write("<body><h1>Welcome to About Us Page</h1></body>");
+        res.write('</html>');
+        return res.end();
+    }
+    if (url==='/node'){
+        res.write('<html>');
+        res.write('<head><title>NODE</title></head>');
+        res.write('<body><h1>Welcome to my Node js Project</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
     res.write('<html>');
-    res.write('<head><title>Hello World</title></head>');
-    res.write('<body><h1>Welcome to my Node js Project</h1></body>');
+    res.write('<head><title>MESSAGE</title></head>');
+    res.write('<body><h1>Message Received</h1></body>');
     res.write('</html>');
     res.end();
 });
